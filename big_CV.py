e70ef6c03afc8e0104fd876b46db0d54b0ce4c9a
SouravGowda <30653224+SouravGowda@users.noreply.github.com>
2018-09-05 11:20:06 +0530

    Add files via upload

---
+++ big_CV.py
@@ -0,0 +1,11 @@
+import cv2
+print(cv2.__version__)
+vidcap = cv2.VideoCapture('big2.mp4')
+success,image = vidcap.read()
+count = 0
+success = True
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)
+  success,image = vidcap.read()
+  print 'Read a new frame: ', success
+  count += 1
